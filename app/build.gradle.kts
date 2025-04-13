73bd689aa20e688a67e1bff1bcb3d3542b294b28
Matthew Nienow <mnienow@pm.me>
2025-04-12 20:57:53 -0500

    Update to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "dev.randombits.intervaltimer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.1.2"
+        versionCode = 13
+        versionName = "1.1.3"
 
         vectorDrawables {
             useSupportLibrary = true
